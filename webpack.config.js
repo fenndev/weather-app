add856a73647d2beb8b1e24c16fff786ac445670
fenndev <fenndev@protonmail.com>
2022-05-08 21:59:50 -0700

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src//js/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Weather App',
+            meta: {viewport: 'width=device-width, initial-scale=1.0'},
+            scriptLoading: 'defer',
+            template: './src/html/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    devtool: 'source-map',
+};
